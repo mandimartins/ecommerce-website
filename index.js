4f70f11f75e81b38ed0574de84918d0e5701d8ed
Amanda Martins <mandimartins23@gmail.com>
2019-07-04 11:49:54 -0300

    listing products

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const db = require('knex')({
     }
 })
 
+db.on('query', query =>{
+    console.log(query)
+})
+
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
@@ -21,7 +25,20 @@ app.get('/', async(req, res)=>{
     console.log(categories)
     res.render('home',{categories})
 })
-
+app.get('/categoria/:id',async(req,res)=>{
+    const categories = await db('categories').select('*')
+    const products = await db('products').select('*').where('id',function(){
+        this
+        .select('categories_products.product_id')
+        .from('categories_products')
+        .whereRaw('categories_products.product_id = products.id')
+        .where('categorie_id',req.params.id)
+    })
+    res.render('category',{
+        categories,
+        products
+    })
+})
 app.listen(port, err =>{
     if(err){
         console.log('nao foi possivel iniciar o servidor',err)
