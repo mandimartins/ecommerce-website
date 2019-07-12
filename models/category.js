e8ab255acf1c756c9442fa73333469801d7bd92e
Amanda Martins <mandimartins23@gmail.com>
2019-07-12 15:20:30 -0300

    Product details

---
+++ models/category.js
@@ -0,0 +1,20 @@
+const slug = require('../utils/slug')
+const getCategories = db => async () =>{
+    const categories = await db('categories').select('*')
+    const categoriesWithSlug = categories.map(category =>{
+        const newCategory = {...category,slug:slug(category.category)}
+        console.log(newCategory)
+        return newCategory
+    })
+    return categoriesWithSlug
+}
+
+const getCategoryById = db => async (id) =>{
+    const category = await db('categories').select('*').where('id',id)
+    return category 
+}
+
+module.exports ={
+    getCategories,
+    getCategoryById
+}
