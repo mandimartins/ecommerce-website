d0e6a8c7a738d0a90a95706a70830a34d5ef7c64
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-07-26 15:23:39 -0300

    Remove seeds from production config

--- knexfile.js
+++ knexfile.js
@@ -31,9 +31,6 @@ module.exports = {
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
     },
-    seeds: {
-      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-    },
     useNullAsDefault: true,
   },
 };
