d2ac9d3ecccb59e5ad2ee9edf939e10d7fc701a8
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-07-26 13:06:00 -0300

    Update product migration

--- src/database/migrations/20200725210030_create_products.js
+++ src/database/migrations/20200725210030_create_products.js
@@ -2,9 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('products', function (table) {
     table.increments('id').notNullable();
     table.string('name', 255).notNullable();
-    table.string('email', 255).notNullable();
-    table.string('description', 255).notNullable();
-
+    table.string('description', 255);
     table.timestamps();
   });
 };
