da782023ac9165b1d316a6dec37267eba80cecd3
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-08-15 14:15:38 -0300

    Move public folder to src

--- src/app/routes/products.js
+++
@@ -1,9 +0,0 @@
-const init = db =>{
-    const router = require('express').Router()
-    const products = require('../controllers/products')(db)
-    router.get('/:id/:slug',products.getProduct)
-
-    return router
-}
-
-module.exports = init
