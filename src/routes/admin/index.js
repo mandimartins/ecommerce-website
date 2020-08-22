98612be92628ba028d237cf68e2c071ef0cfdb76
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-08-22 16:01:13 -0300

    Restric admin route for users with no permission

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,17 +1,21 @@
 const init = (db) => {
   const categories = require('./categories');
-  // const products = require('./products')
 
   const router = require('express').Router();
+
   router.use((req, res, next) => {
     if (req.session.user) {
       if (req.session.user.roles.indexOf('admin') < 0) {
-        res.redirect('/');
+        res.status(403).json({
+          msg: 'Access denied',
+        });
       } else {
         next();
       }
     } else {
-      res.redirect('/login');
+      res.status(403).json({
+        msg: 'Access denied',
+      });
     }
   });
 
