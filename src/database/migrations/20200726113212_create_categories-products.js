b06c90f9ea32b0bb1461036355f2b96b6eb60577
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-08-22 16:12:31 -0300

    Add many to many relationship to categories and products

--- src/database/migrations/20200726113212_create_categories-products.js
+++ src/database/migrations/20200726113212_create_categories-products.js
@@ -1,11 +1,18 @@
 exports.up = function (knex) {
   return knex.schema.createTable('categories_products', (table) => {
     table.increments('id').notNullable();
-    table.integer('product_id').notNullable();
-    table.integer('categorie_id').notNullable();
-
-    table.foreign('product_id').references('id').inTable('products');
-    table.foreign('categorie_id').references('id').inTable('categories');
+    table
+      .integer('product_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('products');
+    table
+      .integer('categorie_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('categories');
   });
 };
 
